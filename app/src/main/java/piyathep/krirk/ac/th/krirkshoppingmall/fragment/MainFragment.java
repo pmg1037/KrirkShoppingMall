b47efc0f96f16d56509ece6ce3c7428c29422da1
Piyathep <pmg_1037@hotmail.com>
2018-03-06 11:43:55 +0700

    Before Luch

---
+++ app/src/main/java/piyathep/krirk/ac/th/krirkshoppingmall/fragment/MainFragment.java
@@ -0,0 +1,24 @@
+package piyathep.krirk.ac.th.krirkshoppingmall.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import piyathep.krirk.ac.th.krirkshoppingmall.R;
+
+/**
+ * Created by Piyathep on 6/3/2561.
+ */
+
+public class MainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}
