cfe0fb3eb0c161c6ee0c45f241651dfaa12f2bb4
Piyathep <pmg_1037@hotmail.com>
2018-03-27 11:52:30 +0700

    Launch

--- app/src/main/java/piyathep/krirk/ac/th/krirkshoppingmall/fragment/MainFragment.java
+++ app/src/main/java/piyathep/krirk/ac/th/krirkshoppingmall/fragment/MainFragment.java
@@ -6,9 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import piyathep.krirk.ac.th.krirkshoppingmall.R;
+import piyathep.krirk.ac.th.krirkshoppingmall.utility.MyAlert;
 
 /**
  * Created by Piyathep on 6/3/2561.
@@ -23,8 +26,37 @@ public class MainFragment extends Fragment {
 //        Register Controller
         registerController();
 
+//        Login Controller
+        loginController();
 
     }   // Main Method
+
+    private void loginController() {
+        Button button = getView().findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText userEditText = getView().findViewById(R.id.edtUser);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+                String userString = userEditText.getText().toString().trim();
+                String passwordString = passwordEditText.getText().toString().trim();
+
+                if (userString.isEmpty()|| passwordString.isEmpty()) {
+//                    Have Space
+                    MyAlert myAlert = new MyAlert(getActivity());
+                    myAlert.myDialog(getString(R.string.title_have_space),
+                            getString(R.string.msg_have_space));
+                } else {
+//                    No Space
+
+
+                }
+
+            }
+        });
+    }
 
     private void registerController() {
         TextView textView = getView().findViewById(R.id.txtNewRegister);
